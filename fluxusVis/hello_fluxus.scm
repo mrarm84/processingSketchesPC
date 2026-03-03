8de65eb6d2348088b8dc0bb52226c57aeeb337f7
JD <jan.danielewicz@gmail.com>
2026-03-03 10:04:46 +0100

    Cleanup: Remove fluxusVis and tools, update .gitignore

--- fluxusVis/hello_fluxus.scm
+++
@@ -1,7 +0,0 @@
-;; Hello Fluxus - A simple test script
-;; Just a rotating cube to verify installation
-(clear)
-(define (animate)
-  (rotate (vector (time) (* 1.1 (time)) (* 1.2 (time))))
-  (draw-cube))
-(every-frame (animate))
